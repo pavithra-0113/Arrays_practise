1fe92676645a067190ed02366563d20f4b37a389
Pavithra13 <pavithranagesh0111@gmail.com>
2026-02-02 08:52:49 +0530

    done

---
+++ src/Reverse.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Reverse {
+
+    static void reverse(int[] arr,int n){
+        int left=0,right=n-1;
+        while(left<right){
+            int temp=arr[left];
+            arr[left]=arr[right];
+            arr[right]=temp;
+            left++;
+            right--;
+        }
+        System.out.print(Arrays.toString(arr));
+    }
+
+
+
+
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+
+        int[] arr=new int[n];
+
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+
+        reverse(arr,n);
+    }
+}
